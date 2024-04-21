f0b9d4d02e80e8306e015cca4be2abc8fb65acc8
Abhishek Choudhari <abhishek007coc@gmail.com>
2024-04-21 22:33:00 +0530

    Separate Algorithms File Added

---
+++ algorithms.js
@@ -0,0 +1,48 @@
+//DSA-Practice
+
+
+//String Questions:-
+
+//Longest Common Prefix
+const longestCommonPrefix = (strs) => {
+    let i = 1; let temp = strs[0];
+
+    while (i < strs.length) { if (!strs[i].startsWith(temp)) { temp = temp.slice(0, -1); } else { i++; } } return temp;
+}
+console.log(longestCommonPrefix(["flower", "flow", "flight"])); // fl
+
+
+//Valid Parentheses
+const isValid = (s) => {
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) { if (s[i] === '(') { stack.push(')'); } else if (s[i] === '{') { stack.push('}'); } else if (s[i] === '[') { stack.push(']') } else if (stack.pop() !== s[i]) { return false; } }
+
+    return !stack.length;
+}
+console.log(isValid('[{()}]')); // true
+
+
+//Valid Palindrome 
+const isValidPalindrome = (s) => {
+    const temp = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+
+    for (let i = 0; i < temp.length; i++) { if (temp[i] !== temp[temp.length - 1 - i]) { return false; } }
+
+    return true;
+}
+console.log(isValidPalindrome('A man, a plan, a canal: Panama')); //true
+
+
+//Isomorphic Strings, Word Pattern(Similar Questions)
+const isIsomorphicString = (s, t) => {
+    if (s.length !== t.length) { return false; }
+
+    const obj = {}; const obj2 = {};
+
+    for (let i = 0; i < s.length; i++) { if ((obj[s[i]] && obj[s[i]] !== t[i]) || (obj2[t[i]] && obj2[t[i]] !== s[i])) { return false; } else { obj[s[i]] = t[i]; obj2[t[i]] = s[i]; } }
+
+    return true;
+
+}
+console.log(isIsomorphicString('egg', 'add')); //true
